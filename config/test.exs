a856693ed1316a411b4a833d61e2bf1573a2474c
Matt Silbernagel <matt@silbernagel.dev>
2021-11-14 21:07:43 -0500

    test config file

---
+++ config/test.exs
@@ -0,0 +1,10 @@
+import Config
+
+config :a_happy_bot,
+  spotify_client_id: "spotify",
+  spotify_client_secret: "secret",
+  spotify_refresh_token: "token",
+  twitch_user: "user",
+  twitch_pass: "password",
+  twitch_chats: ["a_happy_death"],
+  capabilities: ['membership']
