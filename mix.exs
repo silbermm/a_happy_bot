0518ab7070f3d90893ded64bfee0c33461a4563c
Matt <matt@silbernagel.dev>
2022-05-01 21:35:27 -0400

    version 0.1.3

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule AHappyBot.MixProject do
   def project do
     [
       app: :a_happy_bot,
-      version: "0.1.0",
+      version: "0.1.3",
       elixir: "~> 1.12",
       elixirc_paths: elixirc_paths(Mix.env()),
       compilers: Mix.compilers(),
